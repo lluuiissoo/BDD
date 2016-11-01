f1f34cbdf246875c2b7b97ec40df8e4a70c25f7f
lluuiissoo <lluuiissoo@gmail.com>
2016-11-01 15:49:22 -0400

    Sample project using SpecFlow as the BDD engine

---
+++ SpecFlowTest/Calculator.Specs/CalculatorSteps.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using Calculator;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Calculator.Specs
+{
+    [Binding]
+    public class CalculatorSteps
+    {
+        Calculator calculator = new Calculator();
+        private int result;
+
+        [Given(@"I have entered (.*) into the calculator")]
+        public void GivenIHaveEnteredIntoTheCalculator(int number)
+        {
+            calculator.FirstNumber = number;
+        }
+        
+        [Given(@"I have also entered (.*) into the calculator")]
+        public void GivenIHaveAlsoEnteredIntoTheCalculator(int number)
+        {
+            calculator.SecondNumber = number;
+        }
+        
+        [When(@"I press add")]
+        public void WhenIPressAdd()
+        {
+            result = calculator.Add();
+        }
+        
+        [Then(@"the result should be (.*) on the screen")]
+        public void ThenTheResultShouldBeOnTheScreen(int expectedResult)
+        {
+            Assert.AreEqual(result, expectedResult);
+        }
+
+        [When(@"I press substract")]
+        public void WhenIPressSubstract()
+        {
+            result = calculator.Substract();
+        }
+
+    }
+}
---
+++ SpecFlowTest/Calculator/Calculator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class Calculator
+    {
+        public int FirstNumber { private get; set; }
+
+        public int SecondNumber { private get; set; }
+
+        public int Add()
+        {
+            return FirstNumber + SecondNumber;
+        }
+
+        public int Substract()
+        {
+            return FirstNumber - SecondNumber;
+        }
+    }
+}
